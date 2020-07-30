8916d651acf114d93b3543ad04cc565f20097c01
unknown <gguranic@umass.edu>
2020-07-30 15:58:31 -0400

    fixed a bug where between write.csv and tibble by replacing base r write.csv with readr::write_csv

--- data-raw/manuscript_example_data/example_data_for_vignettes.R
+++ data-raw/manuscript_example_data/example_data_for_vignettes.R
@@ -2,12 +2,12 @@ library(dplyr)
 userdata <- FPEMcountry::contraceptive_use %>%
   dplyr::filter(division_numeric_code == 4) %>%
   dplyr::filter(is_in_union == "Y")
-write.csv(userdata, "data-raw/afghanistan_4_married_example.csv")
+readr::write_csv(userdata, "data-raw/manuscript_example_data/afghanistan_4_married_example.csv")
 userpopdata <- FPEMcountry::population_counts %>%
   dplyr::filter(division_numeric_code == 4) %>%
   dplyr::filter(is_in_union == "Y") %>%
   dplyr::filter(mid_year >= 1970, mid_year <= 2030)
-write.csv(userpopdata, "data-raw/afghanistan_4_married_popdata_example.csv")
+readr::write_csv(userpopdata, "data-raw/manuscript_example_data/afghanistan_4_married_popdata_example.csv")
 
 
 # divisions %>%
@@ -19,19 +19,19 @@ write.csv(userpopdata, "data-raw/afghanistan_4_married_popdata_example.csv")
 userdata <- FPEMcountry::contraceptive_use %>%
   dplyr::filter(division_numeric_code == 72) %>%
   dplyr::filter(is_in_union == "Y")
-write.csv(userdata, "data-raw/Botswana_72_married_example.csv")
+readr::write_csv(userdata, "data-raw/manuscript_example_data/Botswana_72_married_example.csv")
 userpopdata <- FPEMcountry::population_counts %>%
   dplyr::filter(division_numeric_code == 72) %>%
   dplyr::filter(is_in_union == "Y") %>%
   dplyr::filter(mid_year >= 1970, mid_year <= 2030)
-write.csv(userpopdata, "data-raw/Botswana_72_married_popdata_example.csv")
+readr::write_csv(userpopdata, "data-raw/manuscript_example_data/Botswana_72_married_popdata_example.csv")
 
 userdata <- FPEMcountry::contraceptive_use %>%
   dplyr::filter(division_numeric_code == 426) %>%
   dplyr::filter(is_in_union == "Y")
-write.csv(userdata, "data-raw/Lesotho_426_married_example.csv")
+readr::write_csv(userdata, "data-raw/manuscript_example_data/Lesotho_426_married_example.csv")
 userpopdata <- FPEMcountry::population_counts %>%
   dplyr::filter(division_numeric_code == 426) %>%
   dplyr::filter(is_in_union == "Y") %>%
   dplyr::filter(mid_year >= 1970, mid_year <= 2030)
-write.csv(userpopdata, "data-raw/Lesotho_426_married_popdata_example.csv")
+readr::write_csv(userpopdata, "data-raw/manuscript_example_data/Lesotho_426_married_popdata_example.csv")
